10a3deea27dba6834a92749919033d518cb4a5b5
nagi0130 <s.hamba@i.softbank.jp>
2024-09-30 17:29:28 +0900

    kadai_mdn_jsの最初のコミット

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const todayDate = today.getDate();
+const todayMonth = today.getMonth() + 1;
+const todayYear = today.getFullYear();
+console.log(todayYear + "年" + todayMonth + "月" + todayDate + "日");
+
+
